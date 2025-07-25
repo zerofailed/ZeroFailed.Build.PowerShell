8bacfa64c9cdfccdc3fe96675310be4f5065c63d
James Dawson <james.dawson@endjin.com>
2025-07-25 17:24:06 +0100

    Tidy-up synopsis comments

--- module/tasks/publish.properties.ps1
+++ module/tasks/publish.properties.ps1
@@ -2,26 +2,16 @@
 # Copyright (c) Endjin Limited. All rights reserved.
 # </copyright>
 
-# PowerShellModulesToPublish must be an array of the following structure:
-# @(
-#     @{
-#         ModulePath = "<path-to-psd1-file>"
-#         FunctionsToExport = @("*")
-#         CmdletsToExport = @()
-#         AliasesToExport = @()
-#     }
-# )
-
 # Synopsis: Configures the PowerShell modules to be published to a PowerShell repository (e.g. PSGallery)
 $PowerShellModulesToPublish = @()
 
-# Synopsis: When true, publishing any PowerShell modules will be skipped. Defaults to false.
+# Synopsis: When true, publishing any PowerShell modules will be skipped.
 $SkipPowerShellPublish = $false
 
-# Synopsis: When true, the PowerShell module will be published even if the version already exists in the repository. Defaults to false.
+# Synopsis: When true, the PowerShell module will be published even if the version already exists in the repository.
 $EnablePowerShellModuleForcePublish = $false
 
-# Synopsis: The name of the PowerShell repository to publish to. Defaults to PSGallery.
+# Synopsis: The name of the PowerShell repository to publish to.
 $PowerShellRepository = "PSGallery"
 
 # Synopsis: The API key to use when publishing to the PowerShell repository.
